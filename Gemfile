714aca0edf980f4bef06590e8ed91f3319426c1e
Josh Lewis <josh.w.lewis@gmail.com>
2014-02-24 21:12:10 -0600

    Fresh start.

--- Gemfile
+++ Gemfile
@@ -1,14 +1,15 @@
 source 'https://rubygems.org'
+ruby '2.1.0'
 
-gem 'sinatra'
-gem 'unitwise'
-gem 'json'
+gem 'sinatra',      '~> 1.4.4'
+gem 'unitwise',     '~> 0.3'
+gem 'json',         '~> 1.8.0'
 
 group :development, :test do
-  gem 'rake'
+  gem 'rake',       '~> 10.1.0'
 end
 
 group :test do
-  gem 'minitest'
-  gem 'rack-test'
+  gem 'minitest',   '~> 2.5.1'
+  gem 'rack-test',  '~> 0.6.2'
 end
--- app.rb
+++ app.rb
@@ -8,17 +8,3 @@ end
 get '/' do
   "It's running. I need to put some kind of instructions here."
 end
-
-get '/atoms.json' do
-  Unitwise::Atom.all.map do |a|
-    { keys: [ a.names, a.primary_code, a.secondary_code, a.symbol].flatten.compact.uniq,
-      dim: a.dim, property: a.property, classification: a.classification }
-  end.to_json
-end
-
-get '/prefixes.json' do
-  Unitwise::Prefix.all.map do |p|
-    { keys: [ p.names, p.primary_code, p.secondary_code, p.symbol].flatten.compact.uniq,
-      scalar: p.scalar }
-  end.to_json
-end
