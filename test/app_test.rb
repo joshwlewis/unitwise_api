b970c42b128b6471624c87b2fe79faaf337b4542
Josh Lewis <josh.w.lewis@gmail.com>
2014-05-31 20:16:39 -0500

    Get specs passing with new urls.

--- test/app_test.rb
+++ test/app_test.rb
@@ -8,23 +8,23 @@ class AppTest < Minitest::Test
   end
 
   def test_units
-    get '/units', q: 'mm', format: 'json'
+    get '/units.json', q: 'mm'
     assert last_response.ok?
     assert_includes last_response.body, 'millimeter'
   end
 
   def test_conversions
-    post '/conversions', { source: { value: 4, unit: 'yard' },
-                           target: { unit: 'm' } }.to_json, format: 'json'
+    post '/conversions.json', { source: { value: 4, unit: 'yard' },
+                                target: { unit: 'm' } }.to_json
     assert last_response.ok?
     assert_equal 'm',                body['result']['unit']
     assert_equal 3.6576000000000004, body['result']['value']
   end
 
   def test_calculations
-    post '/calculations', { left:  { value: 1,  unit: 'mile' },
-                            right: { value: 10, unit: 'km'  },
-                            operator: '+' }.to_json, format: 'json'
+    post '/calculations.json', { left:  { value: 1,  unit: 'mile' },
+                                 right: { value: 10, unit: 'km'  },
+                                 operator: '+' }.to_json
     assert_equal body['result']['unit'], 'mile'
     assert_equal body['result']['value'], 7.213699494949496
   end
