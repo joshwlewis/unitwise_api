ec72314cec0e9349359b330a7dea8ae948a96205
Josh Lewis <josh.w.lewis@gmail.com>
2014-08-31 09:12:51 -0500

    Drop to default workers.

--- config/puma.rb
+++ config/puma.rb
@@ -5,7 +5,6 @@ state_path           '/var/www/api.unitwise.org/shared/tmp/sockets/puma.state'
 activate_control_app 'unix:///var/www/api.unitwise.org/shared/tmp/sockets/pumactl.sock'
 stdout_redirect      '/var/www/api.unitwise.org/shared/log/puma.stdout.log',
                      '/var/www/api.unitwise.org/shared/log/puma.stderr.log', true
-workers              2
 threads              0, 16
 bind                 'unix:///var/www/api.unitwise.org/shared/tmp/sockets/puma.sock'
 daemonize
