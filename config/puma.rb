6670018fe7eefdbabe46b214a771dcb37094e200
Josh Lewis <josh.w.lewis@gmail.com>
2014-08-31 08:55:21 -0500

    Correct socket paths in puma configuration.

--- config/puma.rb
+++ config/puma.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env puma
 tag                  'api.unitwise.org'
-pidfile              '/var/www/api.unitwise.org/tmp/pids/puma.pid'
-state_path           '/var/www/api.unitwise.org/tmp/sockets/puma.state'
-activate_control_app 'unix:///var/www/api.unitwise.org/tmp/sockets/pumactl.sock'
+pidfile              '/var/www/api.unitwise.org/shared/tmp/pids/puma.pid'
+state_path           '/var/www/api.unitwise.org/shared/tmp/sockets/puma.state'
+activate_control_app 'unix:///var/www/api.unitwise.org/shared/tmp/sockets/pumactl.sock'
 stdout_redirect      '/var/www/api.unitwise.org/shared/log/puma.stdout.log',
                      '/var/www/api.unitwise.org/shared/log/puma.stderr.log', true
 workers              2
 threads              0, 16
-bind                 'unix:///var/www/api.unitwise.org/tmp/sockets/puma.sock'
+bind                 'unix:///var/www/api.unitwise.org/shared/tmp/sockets/puma.sock'
 daemonize
 prune_bundler
